b751ac36099f98e7bad1c3c268bc73fff5ce32ab
dch0ph <paul.hodgkinson@durham.ac.uk>
2023-11-01 18:16:11 +0000

    Finish new shield generation

--- scripts/generate_shields.py
+++ scripts/generate_shields.py
@@ -8,49 +8,83 @@ import sys
 #from generate_road_colours import load_settings, generate_colours
 import yaml
 from colormath.color_conversions import convert_color
-from colormath.color_objects import LabColor, LCHabColor, sRGBColor
-from colormath.color_diff import delta_e_cie2000
+from colormath.color_objects import HSLColor, sRGBColor
+#from colormath.color_diff import delta_e_cie2000
 
 verbose = True
 
-class Color:
-    """A color in the CIE lch color space."""
+# class Color:
+#     """A color in the CIE lch color space."""
 
-    def __init__(self, lch_tuple):
-        self.m_lch = LCHabColor(*lch_tuple)
+#     def __init__(self, lch_tuple):
+#         self.m_lch = LCHabColor(*lch_tuple)
 
-    def lch(self):
-        return "Lch({:.0f},{:.0f},{:.0f})".format(*(self.m_lch.get_value_tuple()))
+#     def lch(self):
+#         return "Lch({:.0f},{:.0f},{:.0f})".format(*(self.m_lch.get_value_tuple()))
 
-    def rgb(self):
-        rgb = convert_color(self.m_lch, sRGBColor)
-        if (rgb.rgb_r != rgb.clamped_rgb_r or rgb.rgb_g != rgb.clamped_rgb_g or rgb.rgb_b != rgb.clamped_rgb_b):
-            raise Exception("Colour {} is outside sRGB".format(self.lch()))
-        return rgb.get_rgb_hex()
+#     def rgb(self):
+#         rgb = convert_color(self.m_lch, sRGBColor)
+#         if (rgb.rgb_r != rgb.clamped_rgb_r or rgb.rgb_g != rgb.clamped_rgb_g or rgb.rgb_b != rgb.clamped_rgb_b):
+#             raise Exception("Colour {} is outside sRGB".format(self.lch()))
+#         return rgb.get_rgb_hex()
 
-    def rgb_error(self):
-        return delta_e_cie2000(convert_color(self.m_lch, LabColor),
-                               convert_color(sRGBColor.new_from_rgb_hex(self.rgb()), LabColor))
+#     def rgb_error(self):
+#         return delta_e_cie2000(convert_color(self.m_lch, LabColor),
+#                                convert_color(sRGBColor.new_from_rgb_hex(self.rgb()), LabColor))
+
+
+def to_rgbhex(a):
+    """ Convert (HSL) color to RGB """
+
+    rgb = convert_color(a, sRGBColor)
+    if (rgb.rgb_r != rgb.clamped_rgb_r or rgb.rgb_g != rgb.clamped_rgb_g or rgb.rgb_b != rgb.clamped_rgb_b):
+        raise Exception("Colour {} is outside sRGB".format(a))
+    return rgb.get_rgb_hex()
+
+
+def scale_HSL(orig, alteration):
+    """ Create new HSLColor given an alternation specification. """
+
+    mode, v = alteration
+    newobj = copy.copy(orig)
+    if mode == 'darken':
+        newobj.hsl_l /= v
+        if (newobj.hsl_l < 0.0) or (newobj.hsl_l >1.0):
+            raise ValueError(f"scale_HSL: darken with {v} generated lightness outside gamut: {newobj.hsl_l}")
+    elif mode == 'saturate':
+        newobj.hsl_s *= v
+        if (newobj.hsl_s < 0.0) or (newobj.hsl_s >1.0):
+            raise ValueError(f"scale_HSL: saturate with {v} generated lightness outside gamut: {newobj.hsl_l}")
+    elif mode == 'absolute':
+        pass
+    else:
+        raise KeyError(f"Unknown alteration mode: {mode}")
+    return newobj
+
 
 def parse_color(a):
-    """Return color from string specification (assumed sRGB hex #RRGGBB)"""
+    """Return HSL color from string specification (assumed sRGB hex #RRGGBB)"""
 
-    return sRGBColor.new_from_rgb_hex(a)
+    return convert_color(sRGBColor.new_from_rgb_hex(a), HSLColor)
 
 
 def parse_mode(d, which):
     """ Search dict for colour modification """
 
-    which += '_'
+    whichunderscore = which + '_'
     modetype = None
     for k, v in d.items():
-        if not k.startswith(which):
+        if k == which:
+            modetype = 'absolute'
+            v = parse_color(v)
+            continue
+        if not k.startswith(whichunderscore):
             continue
         if modetype is not None:
-            sys.exit(f"More than one color alteration specified for {which}")
+            sys.exit(f"More than one color specification for {which}")
         if v < 1.0:
             sys.exit(f"Invalid color scaling (must be floating point number >=1.0): {v}")
-        modetype = k[len(which):]
+        modetype = k[len(whichunderscore):]
         if modetype == 'lighten':
             modetype = 'darken'
             v = 1.0/v
@@ -85,13 +119,11 @@ def main():
 #    config['base']['fill'] = '#f1f1f1'
 #    config['base']['stroke_fill'] = '#c6c6c6'
 
-    config['global'] = {}
-
-    config['global']['types'] = list(shieldtypes.keys())
-    config['global']['max_width'] = 11
-    config['global']['max_height'] = 4
-    config['global']['output_dir'] = '../symbols/shields/' # specified relative to the script location
-    config['global']['additional_sizes'] = ['base', 'z16', 'z18']
+    max_width = 11
+    max_height = 4
+    output_dir = '../symbols/shields/' # specified relative to the script location
+#    additional_sizes = ['base', 'z16', 'z18']
+    output_sizes = ['base']
 
     # specific values overwrite config['base'] ones
     for roadtype, roadvalues in shieldtypes.items():
@@ -101,55 +133,54 @@ def main():
         except KeyError:
             sys.exit(f"Missing fill color for road type {roadtype}")
         casingmode = parse_mode(roadvalues, 'casing')
-        if verbose:
-            print(casingmode)
 
-# TODO do colour calculation to determine casing color
-
-        config[roadtype]['fill'] = fill
+        settings['roads'][roadtype]['fill'] = fill
+        settings['roads'][roadtype]['casing'] = scale_HSL(fill, casingmode)
 
     # changes for different size versions
-    config['z16'] = {}
-    config['z18'] = {}
+#    config['z16'] = {}
+#    config['z18'] = {}
+#    config['z16']['font_width'] = 6.1
+#    config['z16']['font_height'] = 14.1
+#    config['z18']['font_width'] = 6.9
+#    config['z18']['font_height'] = 15.1
 
-    config['z16']['font_width'] = 6.1
-    config['z16']['font_height'] = 14.1
-    config['z18']['font_width'] = 6.9
-    config['z18']['font_height'] = 15.1
+    if not os.path.exists(os.path.dirname(output_dir)):
+        os.makedirs(os.path.dirname(output_dir))
 
-    sys.exit("Stop here")
+    vars = settings['shield-geometry']
+    if verbose:
+        print(vars)
+        print(settings['roads'])
 
-    if not os.path.exists(os.path.dirname(config['global']['output_dir'])):
-        os.makedirs(os.path.dirname(config['global']['output_dir']))
-
-    for height in range(1, config['global']['max_height'] + 1):
-        for width in range(1, config['global']['max_width'] + 1):
-            for shield_type in config['global']['types']:
+    for height in range(1, max_height + 1):
+        for width in range(1, max_width + 1):
+            for shield_type, roadsettings in shieldtypes.items():
 
                 # merge base config and specific styles
-                vars = copy.deepcopy(config['base'])
-                if shield_type in config:
-                    for option in config[shield_type]:
-                        vars[option] = config[shield_type][option]
+#                if shield_type in config:
+#                    for option in config[shield_type]:
+#                        vars[option] = config[shield_type][option]
 
-                for shield_size in config['global']['additional_sizes']:
+                for shield_size in output_sizes:
 
-                    if shield_size != 'base':
-                        if shield_size in config:
-                            for option in config[shield_size]:
-                                vars[option] = config[shield_size][option]
+#                    if shield_size != 'base':
+#                        if shield_size in config:
+#                            for option in config[shield_size]:
+#                                vars[option] = config[shield_size][option]
 
                     shield_width = 2 * vars['padding_x'] + math.ceil(vars['font_width'] * width)
                     shield_height = 2 * vars['padding_y'] + math.ceil(vars['font_height'] * height)
+                    stroke_width = vars['stroke_width']
 
                     svg = lxml.etree.Element('svg', nsmap=svgnsmap)
-                    svg.set('width', str(shield_width + vars['stroke_width']))
-                    svg.set('height', str(shield_height + vars['stroke_width']))
-                    svg.set('viewBox', '0 0 ' + str(shield_width + vars['stroke_width']) + ' ' + str(shield_height + vars['stroke_width']))
+                    svg.set('width', str(shield_width + stroke_width))
+                    svg.set('height', str(shield_height + stroke_width))
+                    svg.set('viewBox', '0 0 ' + str(shield_width + stroke_width) + ' ' + str(shield_height + stroke_width))
 
-                    if vars['stroke_width'] > 0:
-                        offset_x = vars['stroke_width'] / 2.0
-                        offset_y = vars['stroke_width'] / 2.0
+                    if stroke_width > 0:
+                        offset_x = stroke_width / 2.0
+                        offset_y = stroke_width / 2.0
                     else:
                         offset_x = 0
                         offset_y = 0
@@ -160,15 +191,18 @@ def main():
                     shield.set('width', str(shield_width))
                     shield.set('height', str(shield_height))
                     if vars['rounded_corners'] > 0:
-                        shield.set('rx', str(vars['rounded_corners']))
-                        shield.set('ry', str(vars['rounded_corners']))
+                        asstr = str(vars['rounded_corners'])
+                        shield.set('rx', asstr)
+                        shield.set('ry', asstr)
 
-                    shield.set('fill', vars['fill'])
+                    fillcolor = to_rgbhex(roadsettings['fill'])
+                    shield.set('fill', fillcolor)
 
                     stroke = ''
-                    if vars['stroke_width'] > 0:
-                        shield.set('stroke', vars['stroke_fill'])
-                        shield.set('stroke-width', str(vars['stroke_width']))
+                    if stroke_width > 0:
+                        strokecolor = to_rgbhex(roadsettings['casing'])
+                        shield.set('stroke', strokecolor)
+                        shield.set('stroke-width', str(stroke_width))
 
                     svg.append(shield)
 
@@ -180,7 +214,7 @@ def main():
 
                     # save file
                     try:
-                        shieldfile = open(os.path.join(os.path.dirname(__file__), config['global']['output_dir'] + filename), 'wb')
+                        shieldfile = open(os.path.join(os.path.dirname(__file__), output_dir + filename), 'wb')
                         shieldfile.write(lxml.etree.tostring(svg, encoding='utf-8', xml_declaration=True, pretty_print=True))
                         shieldfile.close()
                     except IOError:
